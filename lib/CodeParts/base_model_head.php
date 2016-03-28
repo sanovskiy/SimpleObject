9fcd5e5c15af45dbe12e7286e7d878a3ee7d4bd8
Pavel Terentyev <pavel.terentyev@gmail.com>
2016-03-28 17:44:37 +0600

    + Autogeneration header files * version in package.json

---
+++ lib/CodeParts/base_model_head.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * This file created automatically by SimpleObject model generator
+ * DO NOT modify this file beacuse it WILL BE DELETED next time you generate models.
+ * Use child model instead
+ */
+
---
+++ lib/CodeParts/final_model_head.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * This file created automatically by SimpleObject model generator
+ * This file will NOT be deleted on next models generation.
+ */
+
